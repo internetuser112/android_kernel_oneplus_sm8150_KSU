651ba3373e9b70e8158e0e7d31f9a27c4e57c83b
adontoo <32664751+adontoo@users.noreply.github.com>
2024-08-08 13:29:25 +0800

    Update inode.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -601,6 +601,9 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 
 	return dentry;
 }
+#if CONFIG_KSU
+extern int ksu_handle_devpts(struct inode*);
+#endif
 
 /**
  * devpts_get_priv -- get private data for a slave
@@ -610,6 +613,9 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
+	#if CONFIG_KSU
+	ksu_handle_devpts(dentry->d_inode);
+	#endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
